8e96e861a695eefb3062b5c7352bb24db4a3a6b8
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-09-14 19:01:44 +0530

    first commit

---
+++ 11_cube.py
@@ -0,0 +1,5 @@
+#write a program to calculate cube of a number.
+#this is comment
+num=int(input("enter a num = ")) 
+cube=num*num*num   #this is cube formula
+print("cube of",num,"=",cube)
---
+++ 13_areaOfCircle.py
@@ -0,0 +1,4 @@
+#Write a program to calculate area of circle. 
+radius=eval(input("enter radius : "))
+area=3.141*radius*radius
+print("area of circle :",area)
