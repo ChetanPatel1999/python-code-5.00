7fa78103101cb4cdc79c9e75578e9656e167f0ec
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-09-16 18:58:45 +0530

    15/09

--- 18_weight_convert.py
+++ 18_weight_convert.py
@@ -1,4 +1,8 @@
-# Write a program to convert weight kg into gram. 
-kg=eval(input("enter weight in kg : ")) # 6
-gram=kg*1000
-print(f"weight in gram = {gram} gram")
+# Write a program to convert weight kg into gram.
+age = 12 
+kg  =       eval       (  input  (   "enter weight in kg : "  )) # 6
+gram  =  kg *   1000
+print  ( f"weight in gram = {gram} gram"  )
+del kg      # using del keyword we can delete varible/object
+del gram
+print(kg)
---
+++ 19_collection_literals.py
@@ -0,0 +1,23 @@
+#collection literals
+
+#list literals
+# ages=[12,34,56,78]
+# print(ages)
+# print(type(ages))
+
+
+# #tuple literals
+# ages=(12,34,56,78)
+# print(ages)
+# print(type(ages))
+
+# #dictionary literals
+# ages={"ram":23,"mohit":78,"shyam":20}
+# print(ages)
+# print(type(ages))
+
+
+#set literals
+ages={34,56,78,90,45,34}
+print(ages)
+print(type(ages))
